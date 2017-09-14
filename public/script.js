4c7914ede46aab516d243fa067ce1d0ca129ac99
Milos <milosbulatovic3@gmail.com>
2017-09-14 11:14:28 +0200

    trigger default search on page load

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ new Vue({
         total: 0,
         items: [],
         cart: [],
-        newSearch: '',
+        newSearch: 'anime',
         lastSearch: '',
         loading: false
     },
@@ -63,5 +63,8 @@ new Vue({
         currency: function(price) {
             return '$'.concat(price.toFixed(2));
         }
+    },
+    mounted: function() {
+        this.onSubmit();
     }
 });
